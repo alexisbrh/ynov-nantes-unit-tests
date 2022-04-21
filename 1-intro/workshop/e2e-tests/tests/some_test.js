0e0ca14382b522df06baa2aa9115cbb7eb4abe2a
nico3117 <nicobaca17@gmail.com>
2022-04-21 13:40:34 +0200

    test search bachelor informatique ok

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,15 @@
+const CLIENT_URL = `https://www.ynov-nantes.com/`;
+
 Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
-    I.amOnPage('https://www.ynov-nantes.com/');
+    I.amOnPage(CLIENT_URL);
+    
+    I.click({css: 'a.cta'});
+
+    I.fillField('s', 'Bachelor informatique');
+    I.wait(5);
+
+    I.see('Bachelor Informatique');
+    I.wait(5);
 });
