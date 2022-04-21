1789e42624eac81f9e431bfcb0e2de09ec542363
nico3117 <nicobaca17@gmail.com>
2022-04-21 15:59:29 +0200

    test TempTracker insert, get_max, get_min ok

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,28 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+    public function test__insert() {
+        $temp = new TempTracker();
+
+        $temp->insert(10);
+        $this->assertEquals($temp->get_temps(), [10]);
+    }
+
+    public function test__get_max() {
+        $temp = new TempTracker();
+
+        $temp->insert(10);
+        $temp->insert(20);
+
+        $this->assertEquals($temp->get_max(), 20);
+    }
+
+    public function test__get_min() {
+        $temp = new TempTracker();
+
+        $temp->insert(10);
+        $temp->insert(20);
+
+        $this->assertEquals($temp->get_min(), 10);
     }
 }
