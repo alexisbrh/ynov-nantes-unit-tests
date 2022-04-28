89b584c93cca02bf0b006bad1f1093cff323e5ba
Alexis BROHAN <alexisbrohan68@gmail.com>
2022-04-28 10:47:34 +0200

    feat: create class

--- 3-test-driven-development/workshop/src/Minesweeper.js
+++ 3-test-driven-development/workshop/src/Minesweeper.js
@@ -1,8 +1,45 @@
-class MinesweeperService {
-    constructor() {
+class Field {
+    constructor(n, m) {
+        this._n = n;
+        this._m = m;
     }
 
+    get n() {
+        return this._n;
+    }
 
+    set n(value) {
+        this._n = value;
+    }
+
+    get m() {
+        return this._m;
+    }
+
+    set m(value) {
+        this._m = value;
+    }
 }
 
-module.exports = MinesweeperService
+class Board {
+    constructor(fields) {
+        this._fields = fields;
+    }
+
+    get fields() {
+        return this._fields;
+    }
+
+    set fields(value) {
+        this._fields = value;
+    }
+
+    printBoard() {
+
+    }
+}
+
+module.exports = {
+    Board,
+    Field
+}
