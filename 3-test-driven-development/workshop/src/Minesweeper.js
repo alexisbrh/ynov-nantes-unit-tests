3ae8a0a7258be8d639a449cf2b205c844ae9fc59
nico3117 <nicobaca17@gmail.com>
2022-04-29 09:36:28 +0200

    refacto: class Field

--- 3-test-driven-development/workshop/src/Minesweeper.js
+++ 3-test-driven-development/workshop/src/Minesweeper.js
@@ -2,77 +2,80 @@ class Mine {
     constructor(x, y) {
         this._x = x;
         this._y = y;
-    }
+    };
 
     get x() {
         return this._x;
-    }
+    };
 
     set x(value) {
         this._x = value;
-    }
+    };
 
     get y() {
         return this._y;
-    }
+    };
 
     set y(value) {
         this._y = value;
-    }
-}
+    };
+};
 
 class Field {
     constructor(n, m, mines = []) {
-        if(n <= 0 || m <= 0) {
-            throw new Error('N or M should not be negative');
-        }
+        if(n <= 0) {
+            throw new Error('n should not be negative');
+        };
+        if (m <= 0) {
+            throw new Error('m should not be negative');
+        };
 
         this._n = n;
         this._m = m;
-        this._mines = mines
-    }
+        this._mines = mines;
+    };
 
     get n() {
         return this._n;
-    }
+    };
 
     set n(value) {
         this._n = value;
-    }
+    };
 
     get m() {
         return this._m;
-    }
+    };
 
     set m(value) {
         this._m = value;
-    }
+    };
 
     get mines() {
         return this._mines;
-    }
+    };
 
     set mines(value) {
         this._mines = value;
-    }
+    };
 
     calculateNumberOfAdjacentMines(x, y) {
         for(let i = 0; i < this._mines.length; i++) {
             if (this._mines[i].x === x && this._mines[i].y === y) {
                 return '*';
-            }
-        }
+            };
+        };
 
         let numberMines = 0;
         for(let i = x - 1; i <= x + 1; i++) {
             for(let j = y - 1; j <= y + 1;  j++) {
                 if(this._mines.some((mine) => mine.x === i && mine.y === j)) {
                     numberMines += 1;
-                }
-            }
-        }
+                };
+            };
+        };
         return numberMines.toString();
-    }
+    };
 
     printField() {
         let field = '';
