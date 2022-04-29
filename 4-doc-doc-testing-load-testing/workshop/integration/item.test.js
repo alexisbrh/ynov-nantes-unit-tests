f8cf683351e27da9f451b98ae8e20eeafd8df438
Alexis BROHAN <alexisbrohan68@gmail.com>
2022-04-29 16:18:03 +0200

    Add integration tests

---
+++ 4-doc-doc-testing-load-testing/workshop/integration/item.test.js
@@ -0,0 +1,46 @@
+const Todo = require("../toDoModel")
+const mongoose = require('mongoose');
+
+beforeAll(async () => {
+    await mongoose
+        .connect("mongodb://mongo:27017/toDoApp", {
+            useNewUrlParser: true,
+        })
+        .then(() => console.log("MongoDB Connected"))
+        .catch((err) => console.log(err));
+});
+
+test('Persist', async () => {
+    await Todo.create({ text: 'Item 1', done: true }, { text: 'Item 2', done: false });
+});
+
+test('Find all', async () => {
+    const Todos = await Todo.find()
+
+    expect(Todos[0].text).toBe('Item 1');
+    expect(Todos[0].done).toBeTruthy()
+
+    expect(Todos[1].text).toBe('Item 2');
+    expect(Todos[1].done).toBeFalsy()
+});
+
+test('Update by name', async () => {
+    await Todo.findOneAndUpdate({ text: 'Item 1.1' }, { text: 'Item 2.2' })
+});
+
+test('Find one by name', async () => {
+    const Todo = await Todo.findOne({ text: 'Item 2.2' });
+    expect(Todo.text).toBe('Item 2.2');
+    expect(Todo.done).toBeFalsy()
+});
+
+test('Delete', async () => {
+    await Todo.findOneAndDelete({ name: 'Item 2.2' })
+
+    const Todo = await Todo.findOne({ name: 'Item 2.2' });
+    expect(Todo).toBe(null);
+});
+
+afterAll(async () => {
+    await Todo.deleteMany({});
+});
